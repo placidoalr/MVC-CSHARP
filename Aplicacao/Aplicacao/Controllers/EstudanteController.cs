35f5fa6122317d1d8bcd5f65cc429fdd0cbb4ca1
PLACIDO ANTONIO LANGER REINERT <placido.inter@gmail.com>
2018-10-25 09:08:49 -0300

    Initial commit

---
+++ Aplicacao/Aplicacao/Controllers/EstudanteController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Aplicacao.Controllers
+{
+    public class EstudanteController : Controller
+    {
+        // GET: Estudante
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
